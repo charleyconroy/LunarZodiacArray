547aae6fd860a44b0d29e4fde7d2c3e44a2f147d
Charles Conroy <conroycd@bc.edu>
2023-01-30 18:03:28 -0500

    Initial Commit

---
+++ LunarZodiacArray/LunarZodiacArrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunarZodiacArrayApp.swift
+//  LunarZodiacArray
+//
+//  Created by Charley Conroy on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct LunarZodiacArrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
